4512e9a655b48d38b3c4e8dd1e62ac261a0b73b3
Monica Djunaidi <modjun12@gmail.com>
2020-01-07 21:31:50 -0600

    can delete pin from page and firebase

--- src/components/shared/Pin/Pin.js
+++ src/components/shared/Pin/Pin.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import pinShape from '../../../helpers/propz/pinShape';
 
 class Pin extends React.Component {
   static propTypes = {
     pin: pinShape.pinShape,
+    deletePin: PropTypes.func,
+  }
+
+  deletePinEvent = (e) => {
+    e.preventDefault();
+    const { deletePin, pin } = this.props;
+    deletePin(pin.id);
   }
 
   render() {
@@ -15,6 +23,7 @@ class Pin extends React.Component {
         <img src={pin.imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h3 className="card-title">{pin.title}</h3>
+            <button className="btn btn-danger" onClick={this.deletePinEvent}>X</button>
           </div>
         </div>
       </div>
